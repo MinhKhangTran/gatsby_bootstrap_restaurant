10ebb9cf3c08c25687a469dfd92f7bd4efe3fc7a
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2021-10-20 10:22:18 +0200

    :bug: fix viewport

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,21 +15,21 @@ const Hero = () => {
         </div>
         <div className="uhrzeit mt-5 mx-auto border border-danger border-3 p-3">
             <p className="lead">Öffnungszeiten:</p>
-            <div className="zeile d-flex justify-content-between">
+            <div className="lead zeile d-flex justify-content-between">
 <p>Mo</p>
 <div className="zeiten">
   <p>12:00 - 14:30 Uhr</p>
   <p>18:00 - 22:30 Uhr</p>
 </div>
             </div>
-            <div className="zeile d-flex justify-content-between">
+            <div className="lead zeile d-flex justify-content-between">
 <p>Di</p>
 <div className="zeiten">
   <p>Ruhetag</p>
   
 </div>
             </div>
-            <div className="zeile d-flex justify-content-between">
+            <div className="lead zeile d-flex justify-content-between">
 <p>Mi - So</p>
 <div className="zeiten">
   <p>12:00 - 14:30 Uhr</p>
