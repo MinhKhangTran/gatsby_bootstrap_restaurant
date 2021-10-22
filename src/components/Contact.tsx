bff1a0476a62fee30c9b2d75317d9ec397d8f39e
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2021-10-22 09:17:17 +0200

    :bug: delete Leaflet

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -16,23 +16,7 @@ const Contact = () => {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga,
             voluptatem.
           </p>
-          {useHasMounted && (
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          style={{ height: "400px" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      )}
+          
         </Container>
       </section>
     </>
