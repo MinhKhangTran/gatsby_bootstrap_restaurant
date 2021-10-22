6b019045c6b7304cce4c879c3955b063721f2964
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2021-10-22 09:04:26 +0200

    :bug: test useHasMounted

---
+++ src/hooks/useHasMounted.tsx
@@ -0,0 +1,9 @@
+export function useHasMounted() {
+    const [hasMounted, setHasMounted] = React.useState(false);
+    React.useEffect(() => {
+      setHasMounted(true);
+    }, []);
+    return hasMounted;
+  }
+
+  // https://www.joshwcomeau.com/snippets/react-hooks/use-has-mounted/
