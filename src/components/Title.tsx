248976661a81a522033cc118d219c39c4d4925a0
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-23 08:20:46 +0200

    :sparkles: add about page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Title = ({ title }: { title: string }) => {
+const Title = ({ title, heading }: { title: string; heading: boolean }) => {
   return (
     <div className="wrapper">
-      <h2 className="title">{title}</h2>
+      {heading ? (
+        <h1 className="title">{title}</h1>
+      ) : (
+        <h2 className="title">{title}</h2>
+      )}
+
       <div className="underbar_wrapper">
         <div className="underbar underbar_1"></div>
         <div className="underbar underbar_2"></div>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,43 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Layout from "../components/Layout";
 import SEO from "../components/Seo";
+import Title from "../components/Title";
 
 const about = () => {
   return (
     <Layout>
       <SEO title="Über Uns" />
-      <h1 className="bg-danger text-white">Hi Bootstrap and Gatsby</h1>
-      <h1 className="bg-red-100">Hi Bootstrap and Gatsby</h1>
-      <p className="bg-red">Hi Bootstrap and Gatsby</p>
+      <div id="about">
+        <Container>
+          <Row>
+            <Col lg={6}>
+              <StaticImage
+                src="../images/about.jpg"
+                alt="strahlender chefkoch"
+              />
+            </Col>
+            <Col lg={6} className="mt-5 mt-lg-0">
+              <Title heading title="Über uns" />
+              <p className="lead">
+                I'm baby kinfolk microdosing you probably haven't heard of them
+                shaman air plant iPhone fixie semiotics forage. Health goth
+                craft beer direct trade, vexillologist microdosing shabby chic
+                gastropub kale chips seitan migas pok pok cold-pressed kombucha
+                authentic. Williamsburg hexagon occupy YOLO pour-over jianbing
+                aesthetic lomo unicorn taxidermy polaroid sustainable
+                kickstarter. Blue bottle street art mumblecore pork belly
+                shoreditch. Roof party four loko lo-fi shaman marfa flannel
+                tattooed DIY truffaut offal venmo copper mug fanny pack
+                scenester. Authentic affogato put a bird on it, pour-over migas
+                asymmetrical farm-to-table kickstarter.
+              </p>
+              <h2>Midang's Besitzer</h2>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Layout>
   );
 };
